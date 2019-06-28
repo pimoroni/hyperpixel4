e08bbd9e231f5d9631c9d6d6456fcdce628639ea
Phil Howard <phil@gadgetoid.com>
2019-06-28 11:27:25 +0100

    Updated uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,28 +3,22 @@
 SERVICE_NAME="hyperpixel4-init.service"
 SERVICE_PATH="/etc/systemd/system"
 BINARY_NAME="hyperpixel4-init"
+ROTATE_NAME="hyperpixel4-rotate"
 BINARY_PATH="/usr/bin"
 OVERLAY_PATH="/boot/overlays"
 OVERLAY_NAME="hyperpixel4.dtbo"
-OVERLAY_CONFIG="dtoverlay=hyperpixel4"
+OVERLAY_SRC="hyperpixel4.dts"
 
 CONFIG="/boot/config.txt"
 
 CONFIG_LINES=(
 	"dtoverlay=hyperpixel4"
-	"overscan_left=0"
-	"overscan_right=0"
-	"overscan_top=0"
-	"overscan_bottom=0"
-	"framebuffer_width=800"
-	"framebuffer_height=480"
+	"gpio=0-25=a2"
 	"enable_dpi_lcd=1"
-	"display_default_lcd=1"
 	"dpi_group=2"
 	"dpi_mode=87"
 	"dpi_output_format=0x7f216"
-	"display_rotate=3"
-	"hdmi_timings=480 0 10 16 59 800 0 15 113 15 0 0 0 60 0 32000000 6"
+	"dpi_timings=480 0 10 16 59 800 0 15 113 15 0 0 0 60 0 32000000 6"
 )
 
 if [ $(id -u) -ne 0 ]; then
@@ -40,6 +34,13 @@ if [ -f "$SERVICE_PATH/$SERVICE_NAME" ]; then
 	printf "Removed: $SERVICE_PATH/$SERVICE_NAME\n"
 else
 	printf "Skipped: $SERVICE_PATH/$SERVICE_NAME, not installed\n"
+fi
+
+if [ -f "$BINARY_PATH/$ROTATE_NAME" ]; then
+    rm -rf "$BINARY_PATH/$ROTATE_NAME"
+    printf "Removed: $BINARY_PATH/$ROTATE_NAME\n"
+else
+    printf "Skipped: $BINARY_PATH/$ROTATE_NAME, not installed\n"
 fi
 
 if [ -f "$BINARY_PATH/$BINARY_NAME" ]; then
